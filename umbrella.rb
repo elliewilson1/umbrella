747eb6f749c087b02c647081f77882a1282f2819
elliewilson1 <ewilson0@chicagobooth.edu>
2025-04-07 20:47:04 +0000

    geocoding part working

--- umbrella.rb
+++ umbrella.rb
@@ -1,1 +1,38 @@
-# Write your soltuion here!
+# Write your solution here!
+
+require "http"
+require "json"
+require "dotenv/load"
+
+pp "Where are you located?"
+
+user_location = gets.chomp
+# user_location = "Chicago"
+
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+resp = HTTP.get(maps_url)
+
+raw_response = resp.to_s
+
+parsed_response = JSON.parse(raw_response)
+
+results = parsed_response.fetch("results")
+
+first_result = results.at(0)
+
+geo = first_result.fetch("geometry")
+
+loc = geo.fetch("location")
+
+pp latitude = loc.fetch("lat")
+pp longitude = loc.fetch("lng")
+
+google_maps_api_key = ENV.fetch("GMAPS_KEY")
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/"
+
+raw_response = HTTP.get(pirate_weather_url)
